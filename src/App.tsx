f32e0af223f6f41ee51d0143694bd5112611a602
sachie5 <sachamccormack@gmail.com>
2023-12-24 17:34:07 +0000

    added logic in useeffect to filter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.scss';
 import BeerInfo from './containers/BeerInfo/BeerInfo';
 import Main from './containers/Main/Main';
 import Navbar from './containers/Navbar/Navbar';
-import { FormEvent, MouseEvent, useState, useEffect } from 'react';
+import { FormEvent, useState, useEffect, ChangeEvent } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Beer } from './types/types';
 
@@ -12,7 +12,9 @@ const App = () => {
 
     const [beers, setBeers] = useState<Beer[]>([]);
     const [searchTerm, setSearchTerm] = useState<string>("");
-    const [abvNumber, setAbvNumber] = useState<number>(0);
+    const [abvChecked, setAbvChecked] = useState<boolean>(false);
+    const [phChecked, setPhChecked] = useState<boolean>(false);
+    const [dateChecked, setDateChecked] = useState<boolean>(false);
 
 
     const getBeers = async () => {
@@ -22,32 +24,54 @@ const App = () => {
         setBeers(data);
       };
 
-      const handleInput = (event: FormEvent<HTMLInputElement>) => {
+    useEffect(() => {
+        if(searchTerm === "" && !abvChecked && !phChecked && !dateChecked ){
+        getBeers();
+        } else if (searchTerm !== ""){
+                setBeers(prevBeers => prevBeers.filter(beer => {
+                    if (abvChecked && beer.abv > 6){
+                        return beer.name.replace(" ", "").toLowerCase().includes(searchTerm);
+                    } else if(phChecked && beer.ph < 4){
+                    return beer.name.replace(" ", "").toLowerCase().includes(searchTerm);
+                } else if(dateChecked && Number(beer.first_brewed.slice(-4)) < 2010){
+                    return beer.name.replace(" ", "").toLowerCase().includes(searchTerm);
+                }
+                return beer.name.replace(" ", "").toLowerCase().includes(searchTerm);
+        }));
+        }
+    }
+, [abvChecked, phChecked, dateChecked, searchTerm])
+
+
+    const handleInput = (event: FormEvent<HTMLInputElement>) => {
         const newInput = event.currentTarget.value.replace(" ", "").toLowerCase();
         setSearchTerm(newInput);
     };
 
-    useEffect(() => {
-        if(searchTerm === ""){
-        getBeers();
-        } else {
-            setBeers(beers.filter(beer => 
-                beer.name.replace(" ", "").toLowerCase().includes(searchTerm)))
-        }
-    }, [abvNumber, searchTerm])
 
-    const handleClick = (event: MouseEvent<HTMLInputElement>) => {
-        const newValue = event.currentTarget.value;
-        console.log(newValue);
-        setAbvNumber(Number(newValue));
-    } 
+    const handleAbvChange = () => {
+        const newAbvChecked = !abvChecked;
+        return setAbvChecked(newAbvChecked);
+      };
+    
+      const handlePhChange = () => {
+        const newPhChecked = !phChecked;
+        return setPhChecked(newPhChecked);
+      };
+
+      const handleDateChange = () => {
+        const newDateChecked = !dateChecked; 
+        return setDateChecked(newDateChecked);
+      };
+
+      console.log(beers);
 
     return (
         <BrowserRouter>
             <div className="app">
                 <Routes>
                 <Route path="/" element={
-                <><Navbar name="nav" handleInput={handleInput} searchTerm={searchTerm} handleClick={handleClick} value={abvNumber}/>
+                <><Navbar name="nav" handleInput={handleInput} searchTerm={searchTerm} handleAbvChange={handleAbvChange} handleDateChange={handleDateChange} handlePhChange={handlePhChange} abvChecked={abvChecked} phChecked={phChecked} dateChecked={dateChecked}/>
                  <Main beers={beers} /></>
                 } />
                     <Route path="beers/:beerId" element={<BeerInfo beers={beers} />} />
